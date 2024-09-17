78ab58298d44696e08372b624fcd552fe65346f6
Om Panigrahi <149086849+ompanigrahi@users.noreply.github.com>
2024-09-17 22:50:20 +0530

    Update login.js

--- login.js
+++ login.js
@@ -14,7 +14,7 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
 
     if (userID === correctUserID && password === correctPassword) {
         // Redirect to index.html
-        window.location.href = 'login.html';
+        location.href = 'login.html';
     }
     else {
         // Display error message
